94e16923cf2d6b4d5659a896f766117ba2a0ad61
Jesus F Plaz <jfplaz@gmail.com>
2024-11-20 12:30:18 +0000

    Newexcusas

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function excusaAleatoria() {
+    let whoAleatorio = who[Math.floor(Math.random() * who.length)];
+    let actionAleatorio = action[Math.floor(Math.random() * action.length)];
+    let whatAleatorio = what[Math.floor(Math.random() * what.length)];
+    let whenAleatorio = when[Math.floor(Math.random() * when.length)];
+
+    let excusas = `${whoAleatorio} ${actionAleatorio} ${whatAleatorio} ${whenAleatorio}.`;
+    return excusas;
+  }
+  console.log(excusaAleatoria());
+  document.getElementById("excuse").innerHTML = excusaAleatoria();
 };
